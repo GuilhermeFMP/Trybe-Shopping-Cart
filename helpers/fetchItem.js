8a8d39abd4d08d1a534d606668b2c06c01a165c4
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-10-11 17:42:57 -0300

    Requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,7 @@
-const itemUrl = (item) => `https://api.mercadolibre.com/items/${item}`;
+const itemData = (item) => `https://api.mercadolibre.com/items/${item}`;
 
 const fetchItem = async (item) => {
-  const url = itemUrl(item);
+  const url = itemData(item);
   try {
   const request = await fetch(url);
   const js = await request.json();
